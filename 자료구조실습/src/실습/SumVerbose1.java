36fe3695e57082c38bf3311b3a602e5888662d32
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-09-08 15:55:12 +0900

    Create SumVerbose1.java

---
+++ 자료구조실습/src/실습/SumVerbose1.java
@@ -0,0 +1,27 @@
+public class SumVerbose1 {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		int n;
+		
+		System.out.println("1부터 n까지의 합을 구합니다.");
+		
+		do {
+			System.out.print("n : ");
+			n = stdIn.nextInt();
+		} while(n <= 0);
+		
+		int sum = 0;
+		
+		for(int i = 0; i < n; i++) 
+		{
+			sum += (i+1);
+			if(i < n-1)
+				System.out.print((i+1) + " + ");
+			else 
+				System.out.print((i+1) + " = ");
+		}
+		
+		System.out.println(sum);
+	}
+}
