3f076804fc29794521f078fc0a1b16b4c4501294
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-09-18 17:01:45 +0900

    자료구조실습

---
+++ 자료구조실습/src/실습/p220915/star.java
@@ -0,0 +1,80 @@
+package 실습.p220915;
+
+import java.util.Scanner;
+
+public class star {
+	public static void main(String[] args) {
+		// n을 입력 받아 n줄의 직각 삼각형 출력
+		
+		int n;
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			System.out.print("n의 값 : ");
+		// n 입력
+			n = sc.nextInt();
+			
+		} while(n <= 0);
+		
+		// 직각 삼각형 출력
+		System.out.println("\n직각 삼각형");
+		for(int i = 0; i < n; i++) {
+			System.out.printf("%3d ", i);
+			for(int j = 0; j <= i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		// 역 직각 삼각형 출력
+		System.out.println("\n역 직각 삼각형");
+		for(int i = 0; i < n; i++)
+		{
+			System.out.printf("%3d ", i);
+			for(int j = 0; j < n - i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\n길어진 직각 삼각형");
+		for(int i = 0; i < n; i++)
+		{
+			System.out.printf("%3d ", i);
+			for(int j = 0; j < i * 2 + 1; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\n길어진 역 직각 삼각형");
+		for(int i = 0; i < n; i++)
+		{
+			System.out.printf("%3d ", i);
+			for(int j = 0; j < 2 * (n-i) -1; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\n정삼각형");
+		for(int i = 0; i < n; i++)
+		{
+			System.out.printf("%3d ", i);
+			for(int j = 0; j < n - i; j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int j = 0; j <= i * 2; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
