20beea02223d7d9b6d9daaf09395b868281d660d
KIMDAHYUN <74331917+KIMDAHYUN98@users.noreply.github.com>
2022-11-05 23:54:01 +0900

    자료구조 실습

---
+++ 자료구조/src/실습/큐/WDB2.java
@@ -0,0 +1,27 @@
+package 실습.큐;
+
+import java.util.Random;
+
+public class WDB2 {
+	static int NUM = 100;
+
+	public static void main(String args[]) {
+		IntArrayQueue q = new IntArrayQueue(20);
+		Random rand = new Random();
+
+		for (int i = 0; i < 100; i++) {
+			if (q.isFull()) {
+				System.out.println("full 발생");
+				while (!q.isEmpty()) {
+					System.out.printf("[%d] %d 디큐 (front = %d, rear = %d, num = %d)\n", i + 1, q.deque(), q.front,
+							q.rear, q.num);
+				}
+				System.out.println("empty 발생");
+			}
+			int n = rand.nextInt(101);
+			q.enque(n);
+			System.out.printf("[%d] %d 인큐 (front = %d, rear = %d, num = %d)\n", i + 1, n, q.front, q.rear, q.num);
+		} // end of for
+
+	} // end of main
+}
